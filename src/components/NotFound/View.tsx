3f8b2d532cbe946d2563bc2ad32b4f9b89f2cca9
Soulike <soulike@qq.com>
2022-07-03 21:14:31 +0800

    fix: fix export

--- src/components/NotFound/View.tsx
+++ src/components/NotFound/View.tsx
@@ -2,7 +2,7 @@ import notFoundImage from '@/src/static/404_page_not_found_1.svg';
 
 import Style from './Style.module.scss';
 
-export default function NotFound()
+export function NotFound()
 {
     return (
         <div className={Style.NotFound}>
