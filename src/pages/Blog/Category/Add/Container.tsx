7fbb0332e39f612130677e7795418b07abe09e42
Soulike <git_anonymous@foxmail.com>
2022-07-09 11:25:09 +0800

    feat: migrate Category/Add

---
+++ src/pages/Blog/Category/Add/Container.tsx
@@ -0,0 +1,54 @@
+import {message, notification} from 'antd';
+import {NativeButtonProps} from 'antd/lib/button/button';
+import {InputProps} from 'antd/lib/input';
+import {useState} from 'react';
+
+import {Blog} from '@/src/apis';
+
+import {AddView} from './View';
+
+export function Add()
+{
+    const [categoryName, setCategoryName] = useState('');
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const onCategoryNameInputChange: InputProps['onChange'] = e =>
+    {
+        setCategoryName(e.target.value);
+    };
+
+    const initAfterSubmit = () =>
+    {
+        setCategoryName('');
+        setIsSubmitting(false);
+    };
+
+    const onSubmitButtonClick: NativeButtonProps['onClick'] = async e =>
+    {
+        e.preventDefault();
+        if (categoryName.length === 0)
+        {
+            message.warning('分类名不能为空');
+        }
+        else
+        {
+            setIsSubmitting(true);
+            const result = await Blog.Category.add({name: categoryName});
+            setIsSubmitting(false);
+            if (result !== null)
+            {
+                notification.success({message: '分类添加成功'});
+                initAfterSubmit();
+            }
+        }
+    };
+
+    return (
+        <AddView
+            categoryName={categoryName}
+            onCategoryNameInputChange={onCategoryNameInputChange}
+            onSubmitButtonClick={onSubmitButtonClick}
+            isSubmitting={isSubmitting}
+        />
+    );
+}
---
+++ src/pages/Blog/Category/Add/View.tsx
@@ -0,0 +1,32 @@
+import {Button, Input} from 'antd';
+import {NativeButtonProps} from 'antd/lib/button/button';
+import {InputProps} from 'antd/lib/input';
+
+import Style from './Style.module.scss';
+
+interface Props
+{
+    categoryName: string,
+    onCategoryNameInputChange: InputProps['onChange'],
+    onSubmitButtonClick: NativeButtonProps['onClick'],
+    isSubmitting: boolean,
+}
+
+export function AddView(props: Props)
+{
+    const {categoryName, onCategoryNameInputChange, onSubmitButtonClick, isSubmitting} = props;
+    return (
+        <div className={Style.Add}>
+            <Input size={'large'} disabled={isSubmitting}
+                   className={Style.categoryInput}
+                   placeholder={'新分类名'}
+                   value={categoryName}
+                   onChange={onCategoryNameInputChange} />
+            <Button loading={isSubmitting}
+                    disabled={isSubmitting}
+                    type={'primary'}
+                    className={Style.submitButton}
+                    onClick={onSubmitButtonClick}>提交</Button>
+        </div>
+    );
+}
