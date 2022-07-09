41bc7bde450b05cc6a4d462c9c6a8e8aa0c2445f
Soulike <git_anonymous@foxmail.com>
2022-07-09 11:03:11 +0800

    feat: migrate Login

---
+++ src/pages/Login/Container.tsx
@@ -0,0 +1,61 @@
+import {notification} from 'antd';
+import {InputProps} from 'antd/lib/input';
+import {DOMAttributes, useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+
+import {Account} from '@/src/apis';
+import {PAGE_ID, PAGE_ID_TO_ROUTE} from '@/src/config/route';
+
+import {LoginView} from './View';
+
+export function Login()
+{
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    useEffect(() =>
+    {
+        Account.checkSession()
+            .then(res =>
+            {
+                if (res !== null)
+                {
+                    const {isInSession} = res;
+                    if (isInSession)
+                    {
+                        navigate(PAGE_ID_TO_ROUTE[PAGE_ID.MANAGE.INDEX], {replace: true});
+                    }
+                }
+            });
+    }, [navigate]);
+
+    const onUsernameInputChange: InputProps['onChange'] = e =>
+    {
+        setUsername(e.target.value);
+    };
+
+    const onPasswordInputChange: InputProps['onChange'] = e =>
+    {
+        setPassword(e.target.value);
+    };
+
+    const onLoginFormSubmit: DOMAttributes<HTMLFormElement>['onSubmit'] = async e =>
+    {
+        e.preventDefault();
+        const isSuccessful = await Account.login(username, password);
+        if (isSuccessful)
+        {
+            notification.success({message: '登录成功'});
+            navigate(PAGE_ID_TO_ROUTE[PAGE_ID.MANAGE.INDEX]);
+        }
+    };
+
+    return (
+        <LoginView username={username} password={password}
+              onLoginFormSubmit={onLoginFormSubmit}
+              onPasswordInputChange={onPasswordInputChange}
+              onUsernameInputChange={onUsernameInputChange} />
+    );
+}
---
+++ src/pages/Login/View.tsx
@@ -0,0 +1,46 @@
+import {LockOutlined, UserOutlined} from '@ant-design/icons';
+import {Button, Input} from 'antd';
+import {InputProps} from 'antd/lib/input';
+import {DOMAttributes} from 'react';
+
+import web_developer from '../../Static/Login/web_developer.svg';
+import Style from './Style.module.scss';
+
+interface Props
+{
+    username: string,
+    password: string,
+    onUsernameInputChange: InputProps['onChange'],
+    onPasswordInputChange: InputProps['onChange'],
+    onLoginFormSubmit: DOMAttributes<HTMLFormElement>['onSubmit'],
+}
+
+export function LoginView(props: Props)
+{
+    const {username, password, onUsernameInputChange, onPasswordInputChange, onLoginFormSubmit} = props;
+    return (
+        <main className={Style.Login}>
+            <form action={'#'} className={Style.form} onSubmit={onLoginFormSubmit}>
+                <img className={Style.logo} src={web_developer} alt="web_developer_logo" />
+                <Input type={'text'}
+                       size={'large'}
+                       onChange={onUsernameInputChange}
+                       autoFocus={true}
+                       value={username}
+                       placeholder={'用户名'}
+                       prefix={<UserOutlined />} />
+                <Input type={'password'}
+                       size={'large'}
+                       onChange={onPasswordInputChange}
+                       value={password}
+                       placeholder={'密码'}
+                       prefix={<LockOutlined />} />
+                <Button htmlType={'submit'}
+                        className={Style.button}
+                        size={'large'}
+                        type={'primary'}
+                        block={true}>登录</Button>
+            </form>
+        </main>
+    );
+}
