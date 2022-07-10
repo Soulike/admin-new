b45bcaf9e2548fd3871dbb6486236d088ad1c4e1
Soulike <git_anonymous@foxmail.com>
2022-07-10 12:25:13 +0800

    feat: add login check for manage

--- src/components/Frame/Container.tsx
+++ src/components/Frame/Container.tsx
@@ -1,16 +1,26 @@
 import {Modal, notification} from 'antd';
 import {NativeButtonProps} from 'antd/lib/button/button';
 import {ModalFuncProps} from 'antd/lib/modal';
-import {useCallback} from 'react';
+import {useCallback, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 
 import {Account as AccountApi} from '@/src/apis';
+import {Loading} from '@/src/components/Loading';
+import {useIsLoggedIn} from '@/src/hooks/useIsLoggedIn';
 
 import {PAGE_ID, PAGE_ID_TO_ROUTE} from '../../config/route';
 import {FrameView} from './View';
 
 export function Frame() {
     const navigate = useNavigate();
+
+    const {loading, isLoggedIn} = useIsLoggedIn();
+
+    useEffect(() => {
+        if (!loading && !isLoggedIn) {
+            navigate(PAGE_ID_TO_ROUTE[PAGE_ID.LOGIN], {replace: true});
+        }
+    }, [isLoggedIn, loading, navigate]);
 
     const onExitModalOkButtonClick: ModalFuncProps['onOk'] =
         useCallback(async () => {
@@ -21,13 +31,23 @@ export function Frame() {
             navigate(PAGE_ID_TO_ROUTE[PAGE_ID.LOGIN], {replace: true});
         }, [navigate]);
 
-    const onExitButtonClick: NativeButtonProps['onClick'] = (e) => {
-        e.preventDefault();
-        Modal.confirm({
-            content: '确认退出？',
-            onOk: onExitModalOkButtonClick,
-        });
-    };
+    const onExitButtonClick: NativeButtonProps['onClick'] = useCallback<
+        Exclude<NativeButtonProps['onClick'], undefined>
+    >(
+        (e) => {
+            e.preventDefault();
+            Modal.confirm({
+                content: '确认退出？',
+                onOk: onExitModalOkButtonClick,
+            });
+        },
+        [onExitModalOkButtonClick],
+    );
 
-    return <FrameView onExitButtonClick={onExitButtonClick} />;
+    return (
+        <>
+            {loading && <Loading />}
+            {!loading && <FrameView onExitButtonClick={onExitButtonClick} />}
+        </>
+    );
 }
